fba47c2de3e61c70f0c9e35ffb7f423b3d56c340
thunderoo <chris.forbes@biarri.com>
2014-06-11 16:45:30 +1000

    Finish user edit, update, index and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,8 +15,18 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
 
-  it { should be_valid}
+  it { should be_valid }
+  specify { expect(@user.admin?).to eq false}
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    specify { expect(@user.admin?).to eq true}
+  end
 
   describe "when name is not present" do
     before {@user.name = " "}
@@ -110,4 +120,6 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+
+
 end
